0fa7400431ebe998eea2d96e0502e54cc3f905ea
Devi Puspita Dewi <devipuspitadewi@upi.edu>
2021-02-26 09:19:06 +0700

    Aktifitas 1 PBO

---
+++ aktifitas1.py
@@ -0,0 +1,9 @@
+#INPUT
+nama=input("masukkan nama Anda: ")
+umur=19
+#OUTPUT
+print ("")
+print ("====INPUT DAN OUTPUT PADA BAHASA PEMOGRAMAN PYTHON====")
+print ("nama Anda: Devi Puspita Dewi", nama)
+print ("umur Anda: 19 ", umur)
+print ("")
