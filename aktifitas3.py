82daa5bdce25d13e2946345d6882ca1df355cab7
Devi Puspita Dewi <devipuspitadewi@upi.edu>
2021-02-26 09:48:56 +0700

    Devi PD 1905877 Aktifitas 3

---
+++ aktifitas3.py
@@ -0,0 +1,13 @@
+print("percabangan pada bahasa pemograman Python")
+print("pilih bahasa Pemograman")
+print("1. Python")
+print("2. C")
+
+pilih=int(input("pilih menu (1/2) : "))
+
+if pilih ==1:
+    print("bahasa pemograman yang dipilih adalah Python")
+elif pilih ==2:
+    print("bahasa pemograman yang dipilih adalah C")
+else:
+    print("pilihan Anda tidak tersedia!!!")
